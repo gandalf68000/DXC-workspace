9e529ba1310cc94aa39d6cc8fb7f5a5cb248c7f1
Gandalf68002 <gandalf68002@outlook.com>
2024-06-20 12:54:38 -0400

    force build

--- MortgageApplication/cobol/epsmpmt.cbl
+++ MortgageApplication/cobol/epsmpmt.cbl
@@ -14,7 +14,7 @@
       *    OUTPUT:
       *       MONTHLY PAYMENT
       *
-      *    (C) 2019 IBM
+      *    (C) 2019 IBM 
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SOURCE-COMPUTER. FLEX-ES.
